01dd1b4115f4d28f55c9ce33e679987b5ee5716b
ashoksaikumar <ashoksaikumarkondraju@gmail.com>
2022-01-26 23:39:44 +0530

    file mdf by dev1

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
-class hello 
+class hello      #from dev1
 {
-	public static void main()
+	public static void main()     #from dev1
 	{
-		system.out.pritln("hello");
+		system.out.pritln("hello from dev1");
 	}
 }
 
